2d09161d799daa3ad477cd557e977b0d6be4cdfd
DavidRobertDenis <110538874+DavidRobertDenis@users.noreply.github.com>
2023-05-21 20:51:32 +0300

    Add files via upload

---
+++ src/main/java/FirstFunctions.java
@@ -0,0 +1,4 @@
+public class FirstFunctions {
+    public static void main(String [] args) {System.out.println(sayHello());}
+    public static String sayHello() {return "hello";}
+}
---
+++ src/main/java/Functions.java
@@ -0,0 +1,5 @@
+public class Functions {
+    public static void salut(){
+        System.out.println("Salut");
+    }
+}
